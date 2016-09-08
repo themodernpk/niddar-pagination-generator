43a7006f65fa75faa82111ffd5359f70dd02279e
Pradeep Kumar <webreinvent@gmail.com>
2016-09-09 03:09:48 +0530

    v 1.5

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
 'use strict';
-var Url = require("url");
-var colors = require('colors');
 function Pagination(config) {
     this.validate(config);
     this.config = config;
@@ -8,7 +6,7 @@ function Pagination(config) {
     this.config.template = config.template || 'bootstrap';
     this.config.pageParameter = config.pageParameter || 'page';
     this.config.first = isUndefined(config.first) ? 1 : config.first;
-    this.config.displayPages = config.displayPages || 7;
+    this.config.displayPages = config.displayPages || 10;
     this.config.firstLabel = config.firstLabel || '«';
     this.config.previousLabel = config.previousLabel || '‹';
     this.config.middleLabel = config.middleLabel || '...';
@@ -17,8 +15,7 @@ function Pagination(config) {
     this.offset = this.getOffset();
     this.totalPages = this.getTotalPages();
     this.range = this.getRange();
-    this.generator = this.getGenerator();
-    this.template = this.getTemplate();
+    this.render = this.renderTemplate();
 }
 //----------------------------------
 function isUndefined(value) {
@@ -69,56 +66,39 @@ Pagination.prototype.getNext = function () {
     return data;
 }
 //----------------------------------
-Pagination.prototype.getFirstHalf = function () {
-    var data = [];
-    var i = null;
-    var offset = this.getOffset();
-    if (this.config.currentPage <= this.getOffset()) {
-        var start = this.config.first;
-        var end = this.getOffset();
-    } else {
-        var start = this.config.currentPage - offset;
-        var end = start + this.getOffset();
-    }
-    var y = 0;
-    for (i = start; i < end; i++) {
-        data[y] = i;
-        y++;
-    }
-    return data;
+Pagination.prototype.getMiddle = function () {
+    return Math.floor(this.getTotalPages() / 2);
 }
 //----------------------------------
-Pagination.prototype.getSecondtHalf = function () {
-    var data = [];
-    var i = null;
-    var offset = this.getOffset();
-    var lastOffset = this.getTotalPages() - offset;
-    if (this.config.currentPage >= lastOffset) {
-        var start = this.config.currentPage;
-        var end = this.getTotalPages();
+Pagination.prototype.buildUrl = function (page) {
+    var currentUrl = this.config.url;
+    var currentUrlPageString = this.config.pageParameter + "=" + this.config.currentPage;
+    var newPageString = this.config.pageParameter + "=" + page;
+    if (currentUrl.indexOf(currentUrlPageString) > -1) {
+        var newUrl = currentUrl.replace(currentUrlPageString, newPageString);
     } else {
-        var start = this.config.currentPage + 1;
-        var end = start + this.getOffset();
+        var newUrl = currentUrl + "?" + newPageString;
     }
-    var y = 0;
-    for (i = start; i < end; i++) {
-        data[y] = i;
-        y++;
-    }
-    return data;
+    return newUrl;
 }
-//----------------------------------
 //----------------------------------
 Pagination.prototype.getRange = function () {
-    var range = this.getFirstHalf();
-    range.push(this.config.currentPage);
-    var second = this.getSecondtHalf();
-    var range = range.concat(second);
-    return range;
-}
-//----------------------------------
-Pagination.prototype.getGenerator = function () {
     var data = [];
+    var start = 1;
+    var end = 1 + this.config.displayPages;
+    if (this.getTotalPages() < this.config.displayPages) {
+        end = this.getTotalPages();
+    }
+    //if page is greater then offset and less then total pages minum offset
+    if (this.config.currentPage > this.getOffset() && this.config.currentPage < (this.getTotalPages() - this.getOffset())) {
+        start = this.config.currentPage - this.getOffset();
+        end = start + this.config.displayPages;
+    }
+    //if page is greater then offset and less then total pages minum offset
+    if (this.config.currentPage > (this.getTotalPages() - this.getOffset())) {
+        start = this.getTotalPages() - this.config.displayPages;
+        end = this.getTotalPages() + 1;
+    }
     var i = 0;
     if (this.config.currentPage != 1) {
         data[i++] = {
@@ -132,14 +112,20 @@ Pagination.prototype.getGenerator = function () {
             url: this.buildUrl(this.getPrevious())
         }
     }
-    var range = this.getRange();
-    var len = range.length;
-    for (var y = 0; y < len; y++) {
-        data[i++] = {
-            page: range[y],
-            label: range[y],
-            url: this.buildUrl(range[y])
+    for (var y = start; y < end; y++) {
+        if (this.config.currentPage == y) {
+            data[i] = {
+                page: y,
+                url: this.buildUrl(y),
+                isCurrent: true
+            };
+        } else {
+            data[i] = {
+                page: y,
+                url: this.buildUrl(y)
+            };
         }
+        i++;
     }
     if (this.config.currentPage < this.getTotalPages()) {
         data[i++] = {
@@ -156,41 +142,54 @@ Pagination.prototype.getGenerator = function () {
     return data;
 }
 //----------------------------------
-Pagination.prototype.getTemplate = function () {
+Pagination.prototype.renderTemplate = function () {
     var template = this.config.template;
     if (template == "bootstrap") {
         var html = this.getTemplateBoostrap();
+    } else if (template == "foundation") {
+        var html = this.getTemplateFoundation();
     }
     return html;
 }
 //----------------------------------
 Pagination.prototype.getTemplateBoostrap = function () {
-    var generator = this.getGenerator();
+    var range = this.getRange();
     var html = '<nav aria-label="Page navigation"><ul class="pagination">';
-    var range = this.getGenerator();
     var len = range.length;
     for (var y = 0; y < len; y++) {
         var page = range[y];
-        console.log("page", page);
-        html += '<li><a href="' + page.url + '">' + page.label + '</a></li>';
+        var activeClass = "";
+        if ("isCurrent" in page) {
+            activeClass = "active";
+        }
+        if ("label" in page) {
+            html += '<li class="' + activeClass + '"><a href="' + page.url + '">' + page.label + '</a></li>';
+        } else {
+            html += '<li class="' + activeClass + '"><a href="' + page.url + '">' + page.page + '</a></li>';
+        }
     }
     html += '</ul></nav>';
     return html;
 }
 //----------------------------------
-Pagination.prototype.buildUrl = function (page) {
-    var currentUrl = this.config.url;
-    var currentUrlPageString = this.config.pageParameter + "=" + this.config.currentPage;
-    var newPageString = this.config.pageParameter + "=" + page;
-    if (currentUrl.indexOf(currentUrlPageString) > -1) {
-        var newUrl = currentUrl.replace(currentUrlPageString, newPageString);
-    } else {
-        var newUrl = currentUrl + "?" + newPageString;
+Pagination.prototype.getTemplateFoundation = function () {
+    var range = this.getRange();
+    var html = '<ul class="pagination">';
+    var len = range.length;
+    for (var y = 0; y < len; y++) {
+        var page = range[y];
+        var activeClass = "";
+        if ("isCurrent" in page) {
+            activeClass = "current";
+        }
+        if ("label" in page) {
+            html += '<li class="arrow ' + activeClass + '"><a href="' + page.url + '">' + page.label + '</a></li>';
+        } else {
+            html += '<li class="' + activeClass + '"><a href="' + page.url + '">' + page.page + '</a></li>';
+        }
     }
-    return newUrl;
+    html += '</ul>';
+    return html;
 }
-//----------------------------------
-//----------------------------------
-//----------------------------------
 //----------------------------------
 module.exports = Pagination;
